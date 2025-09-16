55436df735826705da683755381342491c5e1d33
felipechurio <felipe.churio@davinci.edu.ar>
2025-09-16 15:55:25 -0300

    Sound

---
+++ Assets/EventManager/EventsType/EventStrings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EventStrings 
+{
+    public const  string EventDeath = "Death Event";
+
+    public const string LifeSubstracter = "Life Substracter";
+}
